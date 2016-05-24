1036dda990212624bac5bd9bcc4f6c5b612d3f61
Sidhartha Mani <sidharthamn@gmail.com>
2016-05-23 20:47:47 -0400

    add tb

---
+++ tb/run_tb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set +x
+
+umount /dev/shm
+mount -t tmpfs -o rw,nosuid,nodev,noexec,relatime,size=524288k shm /dev/shm
+
+(cd /lib/systemd/system/sysinit.target.wants/; for i in *; do [ $i == systemd-tmpfiles-setup.service ] || rm -f $i; done);
+rm -f /lib/systemd/system/multi-user.target.wants/*;
+rm -f /etc/systemd/system/*.wants/*;
+rm -f /lib/systemd/system/local-fs.target.wants/*;
+rm -f /lib/systemd/system/sockets.target.wants/*udev*;
+rm -f /lib/systemd/system/sockets.target.wants/*initctl*;
+rm -f /lib/systemd/system/basic.target.wants/*;
+rm -f /lib/systemd/system/anaconda.target.wants/*;
+
+rpm -Uvh /EMC-ScaleIO-tb-1.32-3455.5.el7.x86_64.rpm
+systemctl enable tb.service
+
+exec $@
